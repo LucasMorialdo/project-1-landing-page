85998014c7d4b1dbc1b1ee8cd3c229e4325fc53b
LucasMorialdo <lucas.morialdo@vp.adv.br>
2026-02-12 00:26:00 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navigation from './components/Navigation'
+import HeroSection from './components/Hero'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Navigation></Navigation>
+      <HeroSection></HeroSection>
+    </div>
+  )
+}
+
+export default App
